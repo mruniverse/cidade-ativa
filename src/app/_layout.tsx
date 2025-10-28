b20310b8b0f49c62940cbc827b0e5543faca3d5d
mruniverse <yurilucaspg@gmail.com>
2025-10-27 23:35:26 -0300

    refactor: enhance RootLayout to support dynamic theming and header visibility

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,23 @@
-import { settings } from '@/src/settings/theme';
-import { Stack } from 'expo-router';
+import { darkTheme, lightTheme, settings } from '@/src/settings/theme';
+import { Stack, usePathname } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 export default function RootLayout() {
+  const pathname = usePathname();
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? darkTheme : lightTheme;
+
+  function shouldShowHeader() {
+    const excludePaths = ['index'];
+    return !!excludePaths.includes(pathname);
+  }
+
   return (
-    <PaperProvider settings={settings}>
-      <Stack />
-      <StatusBar />
+    <PaperProvider theme={theme} settings={settings}>
+      <Stack screenOptions={{ headerShown: shouldShowHeader() }} />
+      <StatusBar style={colorScheme === 'dark' ? 'dark' : 'light'} />
     </PaperProvider>
   );
 }
