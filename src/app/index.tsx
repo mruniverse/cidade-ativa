ea43915178554481f1c6d3e2e5fc52122d17ffb4
mruniverse <yurilucaspg@gmail.com>
2025-11-02 15:07:48 -0300

    feat: implement MapViewComponent with location permissions and marker handling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,90 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { FAB as Fab, useTheme } from 'react-native-paper';
-import { PERMISSIONS, request } from 'react-native-permissions';
-import MarkerComponent, {
-  MarkerComponentProps,
-} from '../components/MarkerComponent';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+import { FAB as Fab } from 'react-native-paper';
+import MapViewComponent from '../components/MapViewComponent';
 
 export default function Index() {
-  const theme = useTheme();
-  const [markers, setMarkers] = React.useState<MarkerComponentProps[]>([
-    {
-      coordinate: { latitude: -24.0438, longitude: -52.3811 },
-      title: 'Marker 1',
-      description: 'Description of Marker 1',
-    },
-  ]);
-
-  const initialRegion = {
-    latitude: -24.0438,
-    longitude: -52.3811,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
-
-  useEffect(() => {
-    request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(result => {
-      console.log('Location permission result:', result);
-    });
-  }, []);
-
-  function handleMapPress(event: any) {
-    const coordinate = event.nativeEvent.coordinate;
-    const newMarker: MarkerComponentProps = {
-      coordinate: {
-        latitude: coordinate.latitude,
-        longitude: coordinate.longitude,
-      },
-      title: 'New Marker',
-      description: 'Description of the new marker',
-      pinColor: theme.colors.primary,
-    };
-
-    console.log('Map pressed at coordinate:', newMarker);
-
-    setMarkers([...markers, newMarker]);
+  function handleFabPress() {
+    console.log('FAB Pressed');
   }
 
   return (
     <View style={styles.container}>
       <Fab
-        rippleColor={'rgba(0, 0, 0, 1)'}
+        onPress={handleFabPress}
         icon="plus"
-        style={{
-          position: 'absolute',
-          bottom: 100,
-          right: 16,
-          zIndex: 1,
-        }}
+        style={styles.buttonContainer}
       />
-      <MapView
-        mapPadding={{
-          left: 0,
-          right: 0,
-          top: 38,
-          bottom: 0,
-        }}
-        onPress={handleMapPress}
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        region={initialRegion}
-        showsUserLocation={true}
-      >
-        {markers.map((marker, index) => (
-          <MarkerComponent key={`marker-${index}`} {...marker} />
-        ))}
-      </MapView>
+      <MapViewComponent />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 100,
+    right: 16,
+    zIndex: 1,
+  },
+});
---
+++ src/components/MapViewComponent.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, {
+  MapPressEvent,
+  MapViewProps,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
+import { useTheme } from 'react-native-paper';
+import { PERMISSIONS, request } from 'react-native-permissions';
+import MarkerComponent, { MarkerComponentProps } from './MarkerComponent';
+
+export default function MapViewComponent(props: MapViewProps) {
+  const theme = useTheme();
+  const [markers, setMarkers] = useState<MarkerComponentProps[]>([]);
+
+  const initialRegion = {
+    latitude: -24.0438,
+    longitude: -52.3811,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  useEffect(() => {
+    request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(result => {
+      console.log('Location permission result:', result);
+    });
+
+    setMarkers([
+      {
+        id: 'initial_marker',
+        coordinate: {
+          latitude: -24.0438,
+          longitude: -52.3811,
+        },
+        title: 'Bem-vindo!',
+        description: 'Este é o ponto inicial do mapa.',
+      },
+    ]);
+  }, []);
+
+  function handleMapPress(event: MapPressEvent) {
+    const coordinate = event.nativeEvent.coordinate;
+    const newMarker: MarkerComponentProps = {
+      id: coordinate.latitude + '_' + coordinate.longitude,
+      coordinate: {
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude,
+      },
+      title: 'New Marker',
+      description: 'Description of the new marker',
+      pinColor: theme.colors.primary,
+    };
+
+    console.log('Map pressed at coordinate:', newMarker);
+
+    setMarkers([...markers, newMarker]);
+  }
+
+  return (
+    <MapView
+      {...props}
+      mapPadding={{
+        left: 0,
+        right: 0,
+        top: 38,
+        bottom: 0,
+      }}
+      onPress={handleMapPress}
+      style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      region={initialRegion}
+      showsUserLocation={true}
+    >
+      {markers.map(marker => (
+        <MarkerComponent key={marker.id} {...marker} />
+      ))}
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
