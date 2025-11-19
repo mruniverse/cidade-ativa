4e6d422aa4c7f3d5df853db919a94b0e33418ffc
mruniverse <yurilucaspg@gmail.com>
2025-11-19 00:42:05 -0300

    chore: update dependencies and devDependencies to latest versions
    - Upgraded @expo/vector-icons to ^15.0.3
    - Downgraded @react-native-picker/picker to 2.11.1
    - Upgraded expo and related packages to their latest versions
    - Upgraded react and react-dom to 19.1.0
    - Upgraded react-native to 0.81.5
    - Updated react-native-gesture-handler, react-native-reanimated, react-native-screens, react-native-svg, react-native-web, and react-native-webview to their latest versions
    - Updated devDependencies including @types/react, eslint-config-expo, and typescript

--- app.config.ts
+++ app.config.ts
@@ -30,6 +30,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   plugins: [
     'expo-router',
+    'expo-font',
+    'expo-web-browser',
     [
       'expo-splash-screen',
       {
